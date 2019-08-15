b88bc52938901dbe4fa0733a16a542b4e8de2dde
caiJava <1050440470@qq.com>
2019-08-15 09:10:08 +0800

    update file, animation, clear bug

--- h5/SortAnimation/SelectionSort/main.js
+++ h5/SortAnimation/SelectionSort/main.js
@@ -1,12 +1,12 @@
 const WIDTH = 800, HEIGHT = 400;
 const COLUMN_WIDTH = 10, COLUMN_MARGIN = 1;
 const LENGTH = ~~(WIDTH / (COLUMN_WIDTH + COLUMN_MARGIN));
-const DEFAULT_COLOR = '#293462', CURRENT_SELECT_COLOR = '#f7be16', CURRENT_MIN_COLOR = '#eb7070', COMPLATE_COLOR = '#ff935c'
+const DEFAULT_COLOR = '#293462', CURRENT_SELECT_COLOR = '#f7be16', CURRENT_MIN_COLOR = '#ed1250', COMPLATE_COLOR = '#ff935c'
 let currentMinIndex = -1;
-let animate = 1;
+let animateTime = 1;
 
 let sortArray = new Array();
-window.onload = function() {
+window.onload = function () {
     let canDom = document.getElementById('canvas');
     canDom.width = WIDTH;
     canDom.height = HEIGHT;
@@ -16,31 +16,36 @@ window.onload = function() {
 }
 
 function sort(ctx) {
-    for (let i = 0; i < LENGTH - 1; i ++) {
-        for (let j = i + 1; j < LENGTH; j ++) {
-            if (sortArray[j] < sortArray[i]) {
-                let item = sortArray[i];
-                sortArray[i] = sortArray[j];
-                sortArray[j] = item;
+    for (let i = 0; i < sortArray.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < sortArray.length; j++) {
+            if (sortArray[j] < sortArray[minIndex]) {
+                minIndex = j;
             }
-            // setTimeout(function() {
-            //     render(ctx, JSON.stringify(sortArray), i + 1, j);
-            // }, (animate ++) * 100)
+            updateView(ctx, copyArray(sortArray), i, j, minIndex);
         }
-        setTimeout(function() {
-            render(ctx, JSON.stringify(sortArray), i + 1, -1);
-        }, (animate ++) * 100)
+        swap(sortArray, i, minIndex);
+        updateView(ctx, copyArray(sortArray), i, -1, -1);
     }
 }
 
-function render(ctx, drowArray, orderIndex, currentCompareIndex) {
-    drowArray = JSON.parse(drowArray);
+function updateView(ctx, array, orderIndex, currentCompareIndex, minIndex) {
+    setTimeout(function () {
+        render(ctx, array, orderIndex, currentCompareIndex, minIndex);
+    }, animateTime++ * 30)
+}
+
+function render(ctx, drowArray, orderIndex, currentCompareIndex, minIndex) {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    for (let i = 0; i < LENGTH; i ++) {
-        if (i <= orderIndex) {
+    for (let i = 0; i < LENGTH; i++) {
+        if (i < orderIndex) {
             drowColumn(ctx, i, drowArray[i], COMPLATE_COLOR);
-        } else if(i == currentCompareIndex) {
+        } else if (i == currentCompareIndex) {
             drowColumn(ctx, i, drowArray[i], CURRENT_SELECT_COLOR);
+        } else if (i == minIndex) {
+            drowColumn(ctx, i, drowArray[i], CURRENT_MIN_COLOR);
+        } else if (i == orderIndex) {
+            drowColumn(ctx, i, drowArray[i], 'black');
         } else {
             drowColumn(ctx, i, drowArray[i], DEFAULT_COLOR)
         }
@@ -48,19 +53,30 @@ function render(ctx, drowArray, orderIndex, currentCompareIndex) {
     }
 }
 
+function swap(array, i, j) {
+    let item = array[i];
+    array[i] = array[j];
+    array[j] = item;
+}
+
+function copyArray(array) {
+    let item = []
+    for (let i = 0; i < array.length; i++) {
+        item[i] = array[i];
+    }
+    return item;
+}
+
 function drowColumn(ctx, index, height, color) {
-    ctx.beginPath();
     let startX = index * (COLUMN_WIDTH + COLUMN_MARGIN);
     let startY = HEIGHT;
     ctx.fillStyle = color;
     ctx.fillRect(startX, startY, COLUMN_WIDTH, - height);
-    ctx.closePath();
 }
 
 function init() {
-    let randomNum;
-    for (let i = 0; i < LENGTH; i ++) {
-        randomNum = ~~(Math.random() * HEIGHT + 1);
+    for (let i = 0; i < LENGTH; i++) {
+        let randomNum = ~~(Math.random() * HEIGHT + 1);
         sortArray[i] = randomNum;
     }
 }
