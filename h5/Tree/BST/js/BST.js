dc78c9b19adb9027368adf4e9b2c5d41c0da5bde
caiJava <1050440470@qq.com>
2019-08-26 15:59:16 +0800

    update

---
+++ h5/Tree/BST/js/BST.js
@@ -0,0 +1,9 @@
+function BST(root) {
+    this.root = root;
+    this.size = 0;
+    this.render = function(ctx, node) {
+        draw()
+        console.log(ctx);
+        console.log('BST.render()');
+    }
+}
---
+++ h5/Tree/BST/js/Node.js
@@ -0,0 +1,8 @@
+function Node(element) {
+    this.element = element;
+    this.left = null;
+    this.right = null;
+    this.add = function() {
+        console.log('aaa');
+    }
+}
