505a40f7ec7803f281f0e99cc3649e92022b129f
caiJava <1050440470@qq.com>
2019-09-10 17:07:08 +0800

    update

--- h5/SortAnimation/MergeSort/main.js
+++ h5/SortAnimation/MergeSort/main.js
@@ -1,35 +1,88 @@
-const WIDTH = 400, HEIGHT = 400;
-const COLUMN_WIDTH = 20, COLUMN_MARGIN = 1;
+const WIDTH = 400, HEIGHT = 600;
+const COLUMN_WIDTH = 10, COLUMN_MARGIN = 1;
 const LENGTH = ~~(WIDTH / (COLUMN_MARGIN + COLUMN_WIDTH));
 
 let sortArray = new Array();
-
 let animationTime = 1;
+let ctx;
+let orderIndex;
 
 window.onload = function() {
     const canvas = document.getElementById('main');
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
-    const ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d');
     init();
-    sort();
-    render(ctx)
+    render(ctx, sortArray, -1, -1, -1);
+    sort(sortArray);
 }
 
-function sort() {
-    let temp = new Array();
-    merge(sortArray, 0, LENGTH / 2, LENGTH, temp)
+function sort(arr) {
+    sortRecursion(arr, 0, LENGTH - 1);
+    animation(ctx, arr, -1, -1, -1);
 }
 
-function merge(array, first, middle, end, temp) {
-
+function sortRecursion(arr, l, r) {
+    if (l >= r) return;
+    let mid = ~~((l + r) / 2);
+    sortRecursion(arr, l, mid);
+    sortRecursion(arr, mid + 1, r);
+    merge(arr, l, mid, r);
 }
 
-function render(ctx) {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    for(let i = 0; i < sortArray.length; i ++) {
-        drawColumn(ctx, i, sortArray[i], '#FFC000');
+function merge(arr, l, mid, r) {
+    let itemArr = copyArray(arr, l, r);
+
+    let i = l; j = mid + 1;
+    for (let k = l; k <= r; k ++) {
+        if (i > mid) {
+            arr[k] = itemArr[j - l];
+            j ++;
+        } else if (j > r) {
+            arr[k] = itemArr[i - l];
+            i ++;
+        } else if (itemArr[i - l] < itemArr[j - l]) {
+            arr[k] = itemArr[i - l];
+            i ++;
+        } else {
+            arr[k] = itemArr[j - l];
+            j ++;
+        }
+        animation(ctx, arr, k, l, r);
     }
+}
+
+function animation(ctx, arr, orderIndex, l, r) {
+    let renderArr = copyArray(arr, 0, arr.length - 1);
+    setTimeout(function() {
+        render(ctx, renderArr, orderIndex, l, r);
+    }, animationTime ++ * 50);
+
+}
+
+function copyArray(arr, start, end) {
+    let itemArr = new Array();
+    let index = 0;
+    for (let i = start; i <= end; i ++, index ++) 
+        itemArr[index] = arr[i];
+    return itemArr;
+}
+
+function render(ctx, arr, orderIndex, l, r) {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    let color;
+    for(let i = 0; i < arr.length; i ++) {
+        if ( i == orderIndex && i == arr.length - 1)
+            color = 'orange'; 
+        else if (i == orderIndex)
+            color = 'red';
+        else if (i >= l && i <= r) 
+            color = 'yellow';
+        else 
+            color = 'orange';
+        drawColumn(ctx, i, arr[i], color);
+    }
+
 }
 
 function drawColumn(ctx, index, height, color) {
